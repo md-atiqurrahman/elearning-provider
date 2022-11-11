96fb1155c169b92043cdc464bdb3c20c26266ddb
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-11-12 01:54:06 +0600

    start the project

---
+++ src/Pages/ReferAndEarn/ReferAndEarn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReferAndEarn = () => {
+    return (
+        <div>
+            <h1>This is the refer and earn page</h1>
+        </div>
+    );
+};
+
+export default ReferAndEarn;
